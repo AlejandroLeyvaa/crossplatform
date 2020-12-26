5ff964dd1591c27e7edea9ba6ea592c8d315c0f6
AlejandroLeyvaa <64874435+AlejandroLeyvaa@users.noreply.github.com>
2020-12-26 15:48:26 -0700

    First commit

---
+++ src/routes/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Home from '../containers/Home';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
+   );
+};
+
+export default App;
